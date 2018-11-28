7446f57478b062c1cf506337ae1f990349c39921
Moon, S.H <35802648+denmark111@users.noreply.github.com>
2018-11-28 22:47:41 +0900

    overhaul

--- Parsers/nasdaq_parser.py
+++ Parsers/nasdaq_parser.py
@@ -6,56 +6,117 @@
 from html.parser import HTMLParser
 import urllib.request, urllib.error
 
-links = []
+datas = []
 
 
-class MyHTMLParser(HTMLParser):
+class linkParser(HTMLParser):
 
     def handle_starttag(self, tag, attrs):
         if tag != 'a':
             return
 
         attr = dict(attrs)
-        links.append(attr)
+        datas.append(attr)
 
 
-def extract(url):
+class articleParser(HTMLParser):
 
-    result = []
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.recording = False
 
-    try:
-        f = urllib.request.urlopen(url)
-        html = f.read()
-        source = html.decode('utf8')
-        f.close()
-    except urllib.error.HTTPError as e:
-        print(e, 'while fetching', url)
-        return
+    def handle_starttag(self, tag, attrs):
+        if tag != 'p':
+            return
+        self.recording = True
 
-    parser = MyHTMLParser()
-    parser.links = []
-    parser.feed(source)
-    for l in links:
-        if 'target' in l:
-            if l['target'] == '_self':
-                result.append(l['href'])
+    def handle_endtag(self, tag):
+        if tag == 'p' and self.recording:
+            self.recording = False
 
-    del result[0]
-    links.clear()
+    def handle_data(self, data):
+        if self.recording:
+            datas.append(data)
 
-    return result
+
+
+class getNewsArticle():
+
+    def __init__(self):
+
+        self.article = []
+
+    def _getParsed(self, url, isArticle=False):
+
+        try:
+            f = urllib.request.urlopen(url)
+            html = f.read()
+            source = html.decode('utf8')
+            f.close()
+        except urllib.error.HTTPError as e:
+            print(e, 'while fetching', url)
+            return
+
+        if not isArticle:
+            parser = linkParser()
+        else:
+            parser = articleParser()
+        parser.feed(source)
+
+    def _getLinks(self, url):
+
+        result = []
+
+        self._getParsed(url)
+
+        for l in datas:
+            if 'target' in l:
+                if l['target'] == '_self':
+                    result.append(l['href'])
+
+        del result[0]
+        datas.clear()
+
+        return result
+
+    def extractArticle(self, url):
+
+        article = ''
+
+        page = self._getLinks(url)
+
+        for links in page:
+            # print(links)
+            datas.clear()
+
+            print(links)
+            self._getParsed(links, True)
+
+            print(datas)
+
+            for d in datas:
+                article += d
+                print(article)
+
+        del article
 
 
 if __name__ in "__main__":
 
     stock_name = 'aapl'
     stock_link = 'https://www.nasdaq.com/symbol/' + stock_name + '/news-headlines?page='
+    parser = getNewsArticle()
+
     output = []
+    i = 1
 
-    for i in range(1, 10):
+    while i < 10:
         target = stock_link + str(i)
+        parser.extractArticle(target)
 
-        print(target)
-
-        output.append(extract(target))
-        print(output[i - 1])
+        i += 1
+        # if not output[i - 1]:
+        #     break
+        # else:
+        #     print(output[i - 1])
+        #     i += 1

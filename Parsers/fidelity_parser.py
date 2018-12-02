f04e1a9bce6a3e1ed4d4e8ac662d8ccb1e68017f
sungm1nn <jjsm4690@naver.com>
2018-12-02 14:15:35 +0900

    sort URL
    sort URL

--- Parsers/fidelity_parser.py
+++ Parsers/fidelity_parser.py
@@ -1,15 +1,15 @@
 from html.parser import HTMLParser
 import urllib.request, urllib.error
 from queue import *
-
-co_ar = LifoQueue() # collect article / stack / ☆☆☆☆☆☆☆☆
-
-tmp_stk = LifoQueue() # article title buffer1/ stack
-tmp_q = Queue() # article title buffer2 /queue
+import string
+import re
+from operator import itemgetter
+import operator
 
 links = []
-output = [] # get page in fidelity.com
-result = [] # result in extract
+output = [] # use for sort URL date
+result = [] # get article title URL in fidelity.com
+temp_result = [] # use for sort URL
 
 class MyHTMLParser(HTMLParser): 
   
@@ -18,46 +18,55 @@ class MyHTMLParser(HTMLParser):
             return 
   
         attr = dict(attrs) 
-        links.append(attr) 
- 
+        links.append(attr)
+        
 def extract(url): 
-   
-      try: 
+
+    try: 
           f = urllib.request.urlopen(url) 
-          html = f.read() 
+          html = f.read()
           source = html.decode('utf8') 
-          f.close() 
-      except urllib.error.HTTPError as e: 
+          f.close()
+    except urllib.error.HTTPError as e: 
           print(e, 'while fetching', url) 
-          return 
- 
-      parser = MyHTMLParser() 
-      parser.links = [] 
-      parser.feed(source) 
-      for l in links: 
-          if 'target' in l and 'title' in l: 
-              if l['target'] == '_top': 
-                  result.append(l['href']) 
-  
-      del result[0] 
-      links.clear() 
-  
-      return result 
- 
+          return
+
+    parser = MyHTMLParser() 
+    parser.links = [] 
+    parser.feed(source) 
+      
+    for l in links: 
+        if 'target' in l and 'title' in l:  
+            if l['target'] == '_top': 
+                if l['href']!='https://www.fidelity.com/sector-investing/overview' : 
+                   result.append(l['href']) 
+                   #output.append(l['href'].strip('https://www.fidelity.com/news/article/compony-news' + 'https://www.fidelity.com/news/article/default' + 'https://www.fidelity.com/news/article/us-markets' + 'https://www.fidelity.com/news/article/investing-ideas' + 'https://www.fidelity.com/news/article/mergers-and-quisition' + 'https://www.fidelity.com/news/article/technology'))
+                   output.append(l['href'].split('/')[6])
+
+    links.clear() 
+
+    return None 
+
 if __name__ in "__main__": 
   
       stock_name = 'amzn' 
-      pagenum = 0
-      stock_link1 = 'https://search.fidelity.com/search/getNewsSearchResults?question=' + stock_name + '&originatingpage=NSRP&NSRPpageSelected='
+      pagenum = 0 
+      stock_link1 = 'https://search.fidelity.com/search/getNewsSearchResults?question=' + stock_name + '&originatingpage=NSRP&NSRPpageSelected=' 
       stock_link2 =  '&navState=root%7Croot-' 
       stock_link3 = '-10%7C0&binningState=&sortBy=&sourceBoxState=&bundleName=news-bundle' 
 
   
       for pagenum in range(0, 10): 
-          target = stock_link1 + str(pagenum+1)+ stock_link2 + str(pagenum * 10) + stock_link3
+          target = stock_link1 + str(pagenum+1)+ stock_link2 + str(pagenum*10) + stock_link3 
             
-          print(target) 
+          extract(target) 
+      # temp_result = result       
+      temp_result = result.copy()
+      #sort url date
+      output.sort()
+      #sort url in chronological order 
+      for i in range(0,len(output)) :
+          result[i]=temp_result[int(output[i].split('=')[1])-1]
           
-          output.append(extract(target))
-          
-          print(output[pagenum]) 
+      for i in range(0,100):    
+          print(result[i])

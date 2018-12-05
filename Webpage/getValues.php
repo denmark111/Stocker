bcdd5fb8e6f03380f3a34f817daebb816a013b9f
Moon, S.H <35802648+denmark111@users.noreply.github.com>
2018-12-05 12:18:27 +0900

    php refresh

--- Webpage/getValues.php
+++ Webpage/getValues.php
@@ -1,35 +1,66 @@
 <?php
+	echo "<p>Hello World</p>";
 
-$db_host = "localhost";
-$db_user = "root";
-$db_passwd = "confidential";
-$db_name = "test";
+	$stock_name = htmlspecialchars($_POST["stock-name"]);
+	$parser_type = htmlspecialchars($_POST["type"]);
 
-$mysqli = new mysqli($db_host, $db_user, $db_passwd, $db_name);
+	echo "<p>" . $stock_name . "</p>";
 
-if ($mysqli)
-{
-    echo "<p>MySQL Alive!!</p>"
+	if ($parser_type === 1)
+	{
+		$parser = "nasdaq.py";
+	}
+	else if ($parser_type === 2)
+	{
+		$parser = "fidelity.py";
+	}
+	else if ($parser_type === 3)
+	{
+		$parser = "seeking.py";
+	}
+	else
+	{
+		header("Location: " . $_SERVER["HTTP_REFERER"]);
+	}
+	$command = escapeshellcmd("python3 ./parsers/" . $parser . " " . $stock_name);
+	$output = shell_exec($command);
 
-    $sql = 'SELECT * FROM test';
-    $res = $mysqli->query($sql);
+	if ($output === "Failed")
+	{
+		header("Location: " . $_SERVER["HTTP_REFERER"]);
+	}
 
-    if ($res->num_rows > 0)
-    {
-        while ($row = $res->fetch_assoc())
-        {
-            echo "$row["timeStamp"] . $row["positive"]";
-        }
-    }
-    else
-    {
-        echo "0 Results";
-    }
-}
-else
-{
-    echo "Failed to access database";
-}
+	$db_host = "localhost";
+	$db_user = "root";
+	$db_passwd = "12341234";
+	$db_name = "test";
 
-$mysqli->close();
+	$mysqli = new mysqli($db_host, $db_user, $db_passwd, $db_name);
+
+	if ($mysqli)
+	{
+		echo "<p>MySQL Success</p>";
+
+		$sql = "SELECT * FROM result WHERE stock_name = " . $stock_name;
+
+		if ($res = $mysqli->query($sql))
+		{
+			while ($row = $res->fetch_assoc())
+			{
+				echo $row["timeStamp"] . " " . $row["positive"];
+			}
+		}
+		else
+		{
+			echo "No result to show";
+		}
+
+		$res->free();
+	}
+	else
+	{
+		echo "<p>MySQL Fail</p>";
+	}
+
+	$mysqli->close();
 ?>

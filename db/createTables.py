dc830a8b8dd516052f99fda2bb9240c9717dde0c
sungm1nn <jjsm4690@naver.com>
2018-12-08 16:51:31 +0900

    refresh

--- db/createTables.py
+++ db/createTables.py
@@ -6,31 +6,41 @@ conn = pymysql.connect(
         host='localhost',
         user='root',
         password='12341234',
-        db='STOCKER')
+        db='STOCKER',
+        autocommit=True)
 
 cursor=conn.cursor()
 sql = '''
             CREATE TABLE seeking (
                 title varchar(255) NOT NULL PRIMARY KEY,
-                date varchar(255) NOT NULL,
-                content text(65535) NOT NULL,
-                url varchar(255) NOT NULL
+                stock varchar(255) NOT NULL,
+                positive varchar(255),
+                negative varchar(255),
+                mixed varchar(255),
+                neutral varchar(255),
+                keyword varchar(255)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 '''
 sql2='''
             CREATE TABLE nasdaq (
                 title varchar(255) NOT NULL PRIMARY KEY,
-                date varchar(255) NOT NULL,
-                content text(65535) NOT NULL,
-                url varchar(255) NOT NULL
+                stock varchar(255) NOT NULL,
+                positive varchar(255),
+                negative varchar(255),
+                mixed varchar(255),
+                neutral varchar(255),
+                keyword varchar(255)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 '''
 sql3='''
             CREATE TABLE fidelity (
                 title varchar(255) NOT NULL PRIMARY KEY,
-                date varchar(255) NOT NULL,
-                content text(65535) NOT NULL,
-                url varchar(255) NOT NULL
+                stock varchar(255) NOT NULL,
+                positive varchar(255),
+                negative varchar(255),
+                mixed varchar(255),
+                neutral varchar(255),
+                keyword varchar(255)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 '''
 cursor.execute(sql)
